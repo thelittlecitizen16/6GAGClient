9853fbe7034481fedefb7d4864bcbf99cee85469
thelittlecitizen16 <thelittlecitizen16@gmail.com>
2020-10-01 18:19:34 +0300

    add config

--- src/application/gagsList/mostPopular.js
+++ src/application/gagsList/mostPopular.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import GagList from '../gagList';
 import '../style/gag.css';
+import config from '../../config.json'
 
 
 
 function MostPopular(props) {
     let mostPopular = props.gags.sort((a, b) => b["likes"] - a["likes"]);
-    mostPopular= mostPopular.slice(0,5);
+    mostPopular= mostPopular.slice(0,config.popularNumber);
    
     return (
         <GagList setGags={props.setGags}  gags={ mostPopular}/>
